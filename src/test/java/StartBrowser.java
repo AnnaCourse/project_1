7d569e20f3fa07f40471eadb37d79293a368d420
asus <ermolaeva.a81@gmail.com>
2021-02-06 11:12:55 -0500

    new class

---
+++ src/test/java/StartBrowser.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class StartBrowser {
+
+    private static final By SEARCH_FIELD_SELECTOR = By.cssSelector(".search-input__input");
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // System Property for Chrome Driver
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\.."+"\\drivers\\chrome\\chromedriver.exe");
+
+        // Instantiate a ChromeDriver class.
+        WebDriver driver = new ChromeDriver();
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+        // Launch Website
+        driver.navigate().to("http://loblaws.ca");
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.elementToBeClickable(SEARCH_FIELD_SELECTOR));
+
+        WebElement searchField = driver.findElement(SEARCH_FIELD_SELECTOR);
+        searchField.sendKeys("apple");
+        searchField.sendKeys(Keys.ENTER);
+
+
+
+    }
+}
+
